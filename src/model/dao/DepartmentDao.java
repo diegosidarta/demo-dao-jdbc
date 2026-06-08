e72a3c5d5d9a362c80bed58f5bb63b618a283e5c
Diego Sidarta <diegosidarta123@gmail.com>
2026-06-08 14:13:26 -0300

    DepartmentDao implementation and test

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -7,8 +7,8 @@ import java.util.List;
 public interface DepartmentDao {
 
     void insert(Department obj);
-    void updarte(Department obj);
+    void update(Department obj);
     void deleteById(Integer id);
     Department findById(Integer id);
-    List<Department> findaAll();
+    List<Department> findAll();
 }
